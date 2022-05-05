1d50bd5c64c105cf51765816285af608c0f79f2a
caroline <caroline.karolll@hotmail.com>
2022-05-05 11:05:42 -0300

    Add route authentication

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -5,6 +5,26 @@ const AuthController = require('./controllers/AuthController');
 const UserController = require('./controllers/UserController');
 const ContentController = require('./controllers/ContentController');
 const SessionController = require('./controllers/SessionController');
+const jwt = require('jsonwebtoken');
+
+const isAuth = (req, res, next) => {
+    try {
+        const token = req.headers.token;
+        if (!token) return res.status(401).json({
+            success: false,
+            message: 'Unable to authenticate'
+        })
+        const user  = jwt.verify(token, process.env.SECRET_KEY);
+        req.userId = user.userId
+        req.email = user.email
+        next()
+    }catch(e){
+        return res.status(401).json({
+            success: false,
+            message: 'Unable to authenticate'
+        })
+    }
+}
 
 // Login da aplicação utilizando o conceito de Sessões ou JSON Web Token
 Router.post('/auth/validate', AuthController.store);
@@ -13,15 +33,15 @@ Router.post('/session', SessionController.store);
 //a cadastro do usuário. deve apresentar validação de existência dos campos
 //unicidade do login e email.Router.post('/user', AppointmentController.store);
 Router.post('/user', UserController.store);
-Router.get('/user', UserController.index);
+Router.get('/user', isAuth, UserController.index);
 
 //deve ser capaz de publicar conteúdos textuais, imagens ou videos no seu próprio perfil. P
-Router.post('/user/:user_id/content');
-Router.get('/user/:user_id/content');
+Router.post('/user/:user_id/content', isAuth);
+Router.get('/user/:user_id/content', isAuth);
 
 //cada usuário deve ser capaz de realizar buscas nos conteúdos publicados.
-Router.get('/content', ContentController.index);
-Router.post('/content', ContentController.store);
-Router.patch('/content', ContentController.update);
+Router.get('/content', isAuth, ContentController.index);
+Router.post('/content', isAuth, ContentController.store);
+Router.patch('/content', isAuth,  ContentController.update);
 
 module.exports = Router;
