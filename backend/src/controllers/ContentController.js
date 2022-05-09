f65622f45b7730192f7957f7927cb51b9b2bd09d
caroline <caroline.karolll@hotmail.com>
2022-05-08 23:10:09 -0300

    Adiciona barra de pesquisa de posts

--- backend/src/controllers/ContentController.js
+++ backend/src/controllers/ContentController.js
@@ -1,6 +1,11 @@
 const Content = require('../models').Content;
 const User = require('../models').User;
-
+const Sequelize = require('sequelize');
+const Op = Sequelize.Op;
+const operatorsAliases = {
+  $like: Op.like,
+  $not: Op.not
+}
 module.exports = {
   async store (req, res) {
     const { filename, mimeType } = req.body;
@@ -55,17 +60,44 @@ module.exports = {
 
   async index (req, res) {
     try {
+      const { filter } = req.query;
+
       if(req.isAdmin) {
-        const allContent = await Content.findAll()
+
+        if(filter){
+
+          const filtered =  await Content.findAll({ where: { filename: { [Op.like]: filter } } });
+          return res.status(200).json({
+            success: true,
+            data: filtered || [],
+          });
+        }
+        const allContent = await Content.findAll();
+
         return res.status(200).json({
           success: true,
           data: allContent || [],
         }) 
       };
 
+      if(filter){
+        const filtered = Content.findAll({ where:
+          { filename: {
+            [Op.like]: filter 
+          },
+          userId: req.userId,
+          }});
+
+        return res.status(200).json({
+          success: true,
+          data: filtered || [],
+        });
+          
+      }
+
       const content = await Content.findAll({
         where: {
-          userId: req.userId
+          userId: req.userId,
         }
       });
 
